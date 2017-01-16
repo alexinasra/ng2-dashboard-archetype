9a4c985f7f07d15388e91c98d133dbddde41c8f4
Alexi Nasra <alexinasra@gmail.com>
2017-01-16 16:41:05 +0200

    Created index.ts

---
+++ src/app/core/index.ts
@@ -0,0 +1,10 @@
+import { BackendResponse } from './backend-response';
+import { Privilege } from './privilege';
+import { Role } from './role';
+import { Credential } from './credential';
+import { User } from './user';
+import { Account } from './account';
+
+export {
+  BackendResponse, Privilege, Role, Credential, User, Account
+}
